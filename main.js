efcc30b8ac2b4526376f553c7db89df26758ca71
CarmozikaNeto97 <carmozikaneto@outlook.com>
2022-12-05 20:13:56 -0300

    Operador lógico

--- main.js
+++ main.js
@@ -23,18 +23,32 @@ let contador = 0;
 
 //     //console.log(contador);
 // }
+
 for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    
-    //console.log(instrumento);
-
     const idAudio = `#som_${instrumento}`;
-    
-    //console.log(idAudio);
 
     tecla.onclick = function () {
         tocaSom(idAudio)
-    };
-    //console.log(contador);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code);
+        
+        if(evento.code === 'Space'){
+            tecla.classList.add('ativa');
+        }
+
+        if(evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function (evento) {
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.remove('ativa')
+        }
+    }
 }
