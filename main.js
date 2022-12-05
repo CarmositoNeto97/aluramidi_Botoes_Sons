2b202997f5445a40fb316af4099456a0b478bf9c
CarmositoNeto97 <carmozikaneto@outlook.com>
2022-12-05 09:53:07 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+};
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    
+    //console.log(instrumento);
+
+    const idAudio = `#som_${instrumento}`;
+    
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    };
+    contador++;
+
+    //console.log(contador);
+}
