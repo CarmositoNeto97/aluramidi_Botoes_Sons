7ee2986d2d5a6de89b3f22081af8134c973e7c2c
CarmozikaNeto97 <carmozikaneto@outlook.com>
2022-12-05 20:17:17 -0300

    Repetição otimizada com For

--- main.js
+++ main.js
@@ -6,20 +6,49 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
-while(contador < listaDeTeclas.length) {
+// while(contador < listaDeTeclas.length) {
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+    
+//     //console.log(instrumento);
+
+//     const idAudio = `#som_${instrumento}`;
+    
+//     //console.log(idAudio);
+
+//     tecla.onclick = function () {
+//         tocaSom(idAudio)
+//     };
+//     contador++;
+
+//     //console.log(contador);
+// }
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    
-    //console.log(instrumento);
-
     const idAudio = `#som_${instrumento}`;
-    
-    //console.log(idAudio);
 
     tecla.onclick = function () {
         tocaSom(idAudio)
-    };
-    contador++;
+    }
 
-    //console.log(contador);
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code);
+        
+        if(evento.code === 'Space'){
+            tecla.classList.add('ativa');
+        }
+
+        if(evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function (evento) {
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.remove('ativa')
+        }
+    }  
 }
